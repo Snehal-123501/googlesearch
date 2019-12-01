c8148759e80ce45772061f42e5c60fc478774889
Sagar Sapkal <katte.snehal32@gmail.com>
2019-12-01 14:25:23 +0530

    Added some code

--- JavaSelenium/src/com/selenium/basics/googleSearch.java
+++ JavaSelenium/src/com/selenium/basics/googleSearch.java
@@ -2,15 +2,24 @@
 
 package com.selenium.basics;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;    //hover on WebDriver
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;   //hover on ChromeDriver
 
 public class googleSearch {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\sagar\\OneDrive\\Desktop\\Selenium\\chromedriver_win32 (1)\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();	//invoke chrome drivers
 		driver.get("https://www.google.com");   //open google page
+		Thread.sleep(5000);
+		WebElement searchBox=driver.findElement(By.name("q"));   //find web element so return 
+		searchBox.sendKeys("Selenium"); //to enter text to web element
+		searchBox.sendKeys(Keys.TAB);
+		WebElement cl=driver.findElement(By.name("btnk"));
+		cl.click();
 	}
 
 }
